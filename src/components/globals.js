1bb045ce70cccd8243e1dcb638a629cdced8869e
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-09-02 13:45:39 -0700

    Create nav bar
    Borrowed from Nick Van Doorn

---
+++ src/components/globals.js
@@ -0,0 +1,37 @@
+export const colours = {
+  background: '#0484EA',
+  main: '#ffffff'
+}
+
+export const margins = {
+  xsm: 6,
+  sm: 12,
+  md: 28,
+  lg: 50,
+  xl: 75
+}
+
+export const transitions = {
+  hover: '0.4s cubic-bezier(0.7, 0, 0.3, 1)'
+}
+
+export const radius = {
+  sm: 8
+}
+
+export const fontStack = 'Helvetica Neue,Helvetica,Arial,sans-serif;'
+
+export const breakpoints = [
+  {
+    breakpoint: 850,
+    size: 750
+  },
+  {
+    breakpoint: 992,
+    size: 970
+  },
+  {
+    breakpoint: 1170,
+    size: 1100
+  }
+]
---
+++ src/components/nav.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { css } from 'emotion'
+import Link from 'gatsby-link'
+import { margins } from './globals'
+
+const navStyle = css(`
+  display: flex;
+  align-items: center;
+  margin: 0 0 ${margins.md}px 0;
+`)
+
+export const Nav = p => {
+  const linkStyle = css(`
+    padding: 0 ${margins.md}px ${margins.md}px ${margins.md}px;
+    &:not(:last-child) {
+      margin: 0 ${margins.sm}px 0 0
+    }
+    text-decoration: none;
+    border: ${p.hoverBorder};
+    border-color: rgba(0, 0, 0, 0);
+    &:hover {
+      border-color: initial;
+    }
+  `)
+  return (
+    <nav className={navStyle}>
+      {p.links
+        ? p.links.map(({ name, link }) => (
+            <Link
+              className={linkStyle}
+              to={link}
+              key={link}
+              activeStyle={{ borderColor: 'initial' }}
+            >
+              {name}
+            </Link>
+          ))
+        : null}
+    </nav>
+  )
+}
