73466f876b261b7721afd8f03a698a1ed6bb0875
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-09-02 17:58:27 -0700

    Add warning symbol to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,12 @@
 import Link from 'gatsby-link'
 import React from 'react'
+import FA from 'react-fontawesome'
+import 'font-awesome/css/font-awesome.min.css';
 
 const NotFoundPage = () => (
   <div>
     <h1>Page not found</h1>
+    <p style={{ fontSize: '17em' }}><FA name="exclamation-triangle" /></p>
     <p style={{ display: 'inline' }}>You just hit a route that does not exist . . .</p>
     <h2 style={{ display: 'inline', marginLeft: '0.4em' }}>
       <Link to="/">Go home?</Link>
