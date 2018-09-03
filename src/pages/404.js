e8612b75ccbcf4adefc6b92b766b7018213d7abb
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-09-02 17:36:10 -0700

    Add home link to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,13 @@
+import Link from 'gatsby-link'
 import React from 'react'
 
 const NotFoundPage = () => (
   <div>
     <h1>Page not found</h1>
-    <p>You just hit a route that does not exist</p>
+    <p style={{ display: 'inline' }}>You just hit a route that does not exist . . .</p>
+    <h2 style={{ display: 'inline', marginLeft: '0.4em' }}>
+      <Link to="/">Go home?</Link>
+    </h2>
   </div>
 )
 
