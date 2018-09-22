92f427e532e308c5167a558e6fdc168445aa8087
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-09-21 22:44:43 -0700

    Increase space between each paragraph

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -33,8 +33,8 @@ class BlogIndex extends React.Component {
       const date = meta.date
       if (!path || !path.startsWith(include)) return
       return (
-        <div style={{ marginBottom: '2em' }} key={path}>
-          <h1 style={{ marginBottom: '0.5em' }}>
+        <div style={{ marginBottom: '1.5em' }} key={path}>
+          <h1 style={{ marginBottom: '0.25em' }}>
             <Link to={node.fields.slug}>{title}</Link>
           </h1>
           {date ? <span style={{ fontWeight: 'bold' }}>{date}</span> : null }
