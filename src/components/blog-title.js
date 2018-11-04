235d6318149d8a1c02088d9a3f3300204e5ed01e
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-11-04 10:12:12 -0800

    Increase spacing for blog post titles (#131)

--- src/components/blog-title.js
+++ src/components/blog-title.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { margins } from './globals'
 
 export const BlogTitle = p => (
   <div style={{ textAlign: 'right' }}>
     {p.title ?
       <div>
-        <h1 style={{ display: 'inline' }}>{p.title}</h1>
+        <h1 style={{ display: p.icon ? 'inline' : 'block',
+                     margin: `${margins.extraSmall} 0` }}>{p.title}</h1>
         {p.icon ?
          <img style={{ marginLeft: '10px', imageRendering: p.icon.mode }}
               width="55px" height="55px" src={p.icon.image} /> :
