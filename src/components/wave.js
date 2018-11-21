90d29ee383521455b81f9869aa2c31e62efb0ae7
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-11-20 23:07:04 -0800

    CSS wave

--- src/components/wave.js
+++ src/components/wave.js
@@ -2,28 +2,32 @@ import React from 'react'
 import { injectGlobal } from 'emotion'
 
 injectGlobal(`
+  body {
+    overflow-x: hidden;
+  }
+  .wave {
+    background: url(wave.svg);
+    width: 5000px;
+    height: 198px;
+    animation: wave 7s cubic-bezier( 0.36, 0.45, 0.63, 0.53) infinite;
+    transform: translate3d(0, 0, 0);
+  }
+  @keyframes wave {
+    0% {
+      margin-left: 0;
+    }
+    100% {
+      margin-left: -1600px;
+    }
+  }
+
+
   svg {
     font-family: 'Raleway',sans-serif;
   }
 `)
 
 export const Wave = p => (
-  <svg viewBox="0 0 100 20">
-    <defs>
-      <pattern id="wave" x="0" y="0" width="120" height="20" patternUnits="userSpaceOnUse">
-        <path id="wavePath" d="M-40 9 Q-30 7 -20 9 T0 9 T20 9 T40 9 T60 9 T80 9 T100 9 T120 9 V20 H-40z" mask="url(#mask)" fill="#FFF">
-          <animateTransform attributeName="transform"
-                            begin="0s"
-                            dur="6s"
-                            type="translate"
-                            from="0,2.5"
-                            to="40,2.5"
-                            repeatCount="indefinite" />
-        </path>
-      </pattern>
-    </defs>
-    <text textAnchor="middle" x="50" y="13" fontSize="13" fill="#f27052" fillOpacity="1">{p.children}</text>
-    <text textAnchor="middle" x="50" y="13" fontSize="13" fill="url(#wave)"  fillOpacity="1">{p.children}</text>
-    <rect fill="url(#wave)" width="200px"></rect>
-  </svg>
+    <div className="wave"></div>
+
 )
