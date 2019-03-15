026b28371348f8aac75ca095bc3950f81424ed98
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-03-14 19:04:41 -0700

    Convert Org links to Gatsby links

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-layout`,
+    `gatsby-plugin-catch-links`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
