7138ff6523a9909fd4a65ac5e682b010c8a8da9d
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-03-24 16:58:03 -0700

    Use packaged fox-hole

--- build-games.sh
+++ build-games.sh
@@ -14,9 +14,5 @@ cp -r games/whale/export/html5/bin/. static/projects/whale/game
 rm -rf static/projects/whale/game/manifest >> /dev/null
 
 # Fox Hole
-cd games/fox-hole
-yarn install
-yarn build
-cd $ROOT
 mkdir -p static/projects/fox-hole/game
-cp -r games/fox-hole/bin/. static/projects/fox-hole/game
+cp -r node_modules/fox-hole/dist/. static/projects/fox-hole/game
