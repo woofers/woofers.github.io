d6897f74b2975d6093fa161e24f16c9fbaec3496
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-05-14 11:23:04 -0700

    Prevent whale from not scaling on low resolutions

--- src/components/frame.js
+++ src/components/frame.js
@@ -2,14 +2,6 @@ import React from 'react'
 import { css } from '@emotion/core'
 import noHighlight from '../utils/no-highlight'
 
-const container = theme => css`
-  ${noHighlight};
-  text-align: center;
-  margin: ${theme.margins.small} 0;
-  position: relative;
-  padding-top: 56.25%;
-`
-
 const iframe = css`
   position: absolute;
   top: 0;
@@ -18,8 +10,21 @@ const iframe = css`
   height: 100%;
 `
 
-export const Frame = p => (
-  <div css={container}>
-    <iframe title={p.title} src={p.src} css={iframe} frameBorder="0" allowFullScreen />
-  </div>
-)
+export const Frame = p => {
+  const container = theme => css`
+    ${noHighlight};
+    text-align: center;
+    margin: ${theme.margins.small} 0;
+    position: relative;
+    padding-bottom: ${100 / p.aspectRatio}%;
+  `
+  return (
+    <div css={container}>
+      <iframe title={p.title} src={p.src} css={iframe} frameBorder="0" allowFullScreen />
+    </div>
+  )
+}
+
+Frame.defaultProps = {
+  aspectRatio: 16 / 9
+}
--- src/components/game.js
+++ src/components/game.js
@@ -6,18 +6,20 @@ import Pico8 from '../components/pico-8'
 
 const grid = css`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(405px, 1fr));
-  grid-template-rows: 860px;
+  grid-template-columns: repeat(auto-fill, minmax(0px, 463px));
   grid-column-gap: 50px;
   grid-row-gap: 6px;
+  justify-content: center;
 `
 
 export const Game = p => (
-    <div css={p.portrait && p.lang !== "pico" ? grid : ''}>
-      { p.lang === 'pico'
-        ? <Pico8 src={p.src} placeholder={p.placeholder} />
-        : <Frame title={p.title} src={p.src} />
-      }
-      <Content html={p.instruction} />
-    </div>
+  <div css={p.portrait && p.lang !== "pico" ? grid : ''}>
+    { p.lang === 'pico'
+      ? <Pico8 src={p.src} placeholder={p.placeholder} />
+      : <Frame aspectRatio={p.portrait && p.lang !== "pico" ? (9 / 16) : (16 / 9)}
+               title={p.title} src={p.src}
+        />
+    }
+    <Content html={p.instruction} />
+  </div>
 )
