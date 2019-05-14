9edfaf9a87dde1681aab1ec451ee21456e86bef4
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-05-14 12:01:03 -0700

    Use light grey footer color for light theme

--- src/themes/light.js
+++ src/themes/light.js
@@ -2,13 +2,17 @@ import { theme as base, rgba } from './base'
 import merge from 'lodash.merge'
 import cloneDeep from 'lodash.clonedeep'
 
+const link = '#ff7757'
+
 const light = {
   colors: {
     button: rgba.code(0.015),
     background: '#f6f8fa',
-    link: '#ff7757',
+    link,
     text: '#1e1d1f',
-    switchBackground: "#e6cbcb"
+    switchBackground: "#e6cbcb",
+    footerElements: link,
+    footer: '#e3e9ef'
   },
   selections: {
     main: rgba.header(0.8)
