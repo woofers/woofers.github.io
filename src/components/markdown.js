1cdf73881bf670ab13e98f41e18fb42e61b1e658
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-05-23 13:46:20 -0700

    Display images next to projects

--- src/components/markdown.js
+++ src/components/markdown.js
@@ -2,7 +2,9 @@ import React from 'react'
 import remark from 'remark'
 import remark2react from 'remark-react'
 import { css } from '@emotion/core'
+import unist from 'unist-builder'
 import visit from 'unist-util-visit'
+import { selectAll, select } from 'unist-util-select'
 
 const edge = css`
   * {
@@ -14,8 +16,18 @@ const edge = css`
   }
 `
 
+export const onlyImages = () => {
+  return (tree) => unist('paragraph', selectAll('image', tree))
+}
 
-const Markdown = p => {
+export const firstImage = () => {
+  return (tree) => {
+    const image = select('image', tree)
+    return image ? image : unist('paragraph', '')
+  }
+}
+
+export const Markdown = p => {
   const links = options => {
     const visitor = node => {
       const githubLink = /^\.\//g
@@ -25,16 +37,20 @@ const Markdown = p => {
     }
     return (tree) => visit(tree, ['image', 'link', 'linkReference'], visitor)
   }
+  const content = () => {
+    let md = remark().use(remark2react).use(links)
+    for (const filter of p.filters) {
+      md = md.use(filter)
+    }
+    return md.processSync(p.content).contents
+  }
   return (
     <div css={edge}>
-      {
-        remark()
-          .use(remark2react)
-          .use(links)
-          .processSync(p.content).contents
-      }
+      {content()}
     </div>
   )
 }
 
-export default Markdown
+Markdown.defaultProps = {
+  filters: []
+}
