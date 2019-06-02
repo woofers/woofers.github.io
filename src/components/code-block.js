1bbcad4b7d28514f63d73ec12a60d3d2c347c18f
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-06-01 23:16:55 -0700

    Only clone child when mutation is needed for code blocks

--- src/components/code-block.js
+++ src/components/code-block.js
@@ -1,25 +1,20 @@
 import React, { useEffect, useRef } from 'react'
-import cloneDeep from 'lodash.clonedeep'
 import 'prismjs'
 import 'prismjs/components/prism-jsx'
 
 const CodeBlock = p => {
-  const children = cloneDeep(p.children)
-  for (const child of children) {
-    if (child.props.className) {
-      child.props.className = child.props.className.trim()
-      if (child.props.className === 'language-js') child.props.className += 'x'
-    }
-  }
   const ref = useRef()
   useEffect(() => {
     const code = () => ref.current.children[0]
-    const lang = () => code().className
-    if (lang()) window.Prism.highlightElement(code(), false)
+    const lang = code().className
+    if (lang === 'language-js') {
+      code().className = `${lang}x`
+    }
+    if (lang) window.Prism.highlightElement(code(), false)
     ref.current.removeAttribute('class')
   })
   return (
-    <pre ref={ref}>{children}</pre>
+    <pre ref={ref}>{p.children}</pre>
   )
 }
 
