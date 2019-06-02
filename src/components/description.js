b7f8827deffc6e7545685ef1e344ac3dab42f967
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-06-01 23:49:43 -0700

    Remove empty spans from project description

--- src/components/description.js
+++ src/components/description.js
@@ -13,7 +13,7 @@ const Description = ({ text }) => {
     if (lastMatch) {
       i = lastMatch.index + lastMatch[0].length
       const segment = text.substring(i + 1, match.index)
-      desc.push(<span key={`${segment}-${i}-${lastMatch[0]}-text-${text}`}>{segment}</span>)
+      if (segment) desc.push(<span key={`${segment}-${i}-${lastMatch[0]}-text-${text}`}>{segment}</span>)
     }
     const emoji = match[0]
     desc.push(<span key={`${text}-${match[0]}-${match.index}`} ><Emoji emoji={emoji} /> </span>)
@@ -21,7 +21,7 @@ const Description = ({ text }) => {
     lastMatch = match
   }
   const last = text.substring(i + 1, text.length)
-  desc.push(<span key={`${last}-last-${text}`}>{last}</span>)
+  if (last) desc.push(<span key={`${last}-last-${text}`}>{last}</span>)
   return (<div>{desc}</div>)
 }
 
