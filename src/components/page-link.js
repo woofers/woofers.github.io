0177694dff110c80e52361b68689d44937407264
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-06-01 23:56:40 -0700

    Add spacing between buttons on project page

--- src/components/page-link.js
+++ src/components/page-link.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { css } from '@emotion/core'
 import Link from './smart-link'
+import { withTheme } from 'emotion-theming'
 
-const style = css`
+const style = theme => css`
   text-align: right;
+  margin-bottom: ${theme.margins.extraSmall};
 `
 
 const Container = p => p.inline ? (<span css={style}>{p.children}</span>) : (<div css={style}>{p.children}</div>)
@@ -14,4 +16,4 @@ const PageLink = p => (
   </Container>
 )
 
-export default PageLink
+export default withTheme(PageLink)
