603969dfee6bfb5a7493371d561180f7ed68193c
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-08-17 11:02:51 -0700

    Use Ludum Dare badges API rather than react-ludum-dare

--- src/components/ludum-dare.js
+++ src/components/ludum-dare.js
@@ -1,22 +1,13 @@
 import React from 'react'
-import { css } from '@emotion/core'
-import Widget from 'react-ludum-dare'
-import { withTheme } from 'emotion-theming'
 
-const padding = theme => css`
-  td {
-    padding: 0 !important;
-  }
-  h2 {
-    color: #fff !important;
-  }
-  margin-bottom: ${theme.margins.medium};
-`
+const LD = p => {
+  const api = `https://badges.vandoorn.ca/${p.game}/badge.${p.type}`
+  const name = p.game.substring(p.game.indexOf('/') + 1)
+  return <img src={api} alt={`${name} Ludum Dare results`} />
+}
 
-const LD = p => (
-  <div css={padding}>
-    <Widget usePlaceholder={true} {...p} />
-  </div>
-)
+LD.defaultProps = {
+  type: 'svg'
+}
 
-export default withTheme(LD)
+export default LD
