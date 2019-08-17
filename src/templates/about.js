05bfbe8b4bfa483f5b09b67fd707db1ae205bf95
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-08-17 11:08:30 -0700

    Add Twitter link to about page

--- src/templates/about.js
+++ src/templates/about.js
@@ -6,18 +6,19 @@ import { graphql } from 'gatsby'
 import { withTheme } from 'emotion-theming'
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
 import { faScroll } from '@fortawesome/free-solid-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import Button from '../components/button'
 
 class AboutTemplate extends Component {
   render() {
     const post = this.props.data.orgContent
     const { social, resume } = this.props.data.site.siteMetadata
-    const { github } = social
+    const { github, twitter } = social
     const { margins } = this.props.theme
     return (
       <Page title={post.meta.title} site={this.props.data.site.siteMetadata.title}>
         <Profile width={margins.profile} height={margins.profile} >
+          <Button href={twitter.link}><Icon icon={faTwitter}/> {twitter.handle}</Button>
           <Button href={github.link}><Icon icon={faGithub}/> {github.name}</Button>
           <Button href={resume}><Icon icon={faScroll}/> Resume</Button>
         </Profile>
