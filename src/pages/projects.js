8c386bf4314f7018c9c1b2ab935c0de8098e7639
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-03-01 17:43:31 -0800

    Hover img

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -17,6 +17,7 @@ const push = theme => css`
 `
 
 const container = css`
+  position: relative;
   margin-bottom: 10px;
   display: flex;
   justify-content: space-between;
@@ -29,25 +30,13 @@ const start = css`
   max-width: 650px;
 `
 
-const end = css`
-  align-content: flex-end;
-  align-self: center;
-  @media screen and (max-width: 1000px) {
-    margin-right: auto;
-    margin-left: auto;
-  }
-  img {
-    margin: 10px auto;
-    max-height: 600px;
-    display: block;
-    @media screen and (min-width: 1000px) {
-      max-width: 500px;
-      max-height: 405px;
-    }
-  }
-`
-
 class Projects extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {  }
+    this.render = this.render.bind(this)
+  }
+
   render() {
     const { exclude } = this.props.data.site.siteMetadata
     const { title } = this.props.data.site.siteMetadata
@@ -61,17 +50,53 @@ class Projects extends Component {
       const md = dlv(repo.readme, 'text')
       const name = repo.fullName
       if (!name) return null
+      const end = theme => css`
+        z-index: -1;
+        top: 0;
+        right: 0;
+        transition: ${theme.transitions.hover};
+        transition-property: opacity;
+        position: absolute;
+        align-content: flex-end;
+        align-self: center;
+        opacity: 0.75;
+        &[aria-hidden=true] {
+          opacity: 0;
+        }
+        @media screen and (max-width: 1000px) {
+          margin-right: auto;
+          margin-left: auto;
+        }
+        img {
+          margin: 10px auto;
+          max-height: 600px;
+          display: block;
+          @media screen and (min-width: 1000px) {
+            max-width: 500px;
+            max-height: 405px;
+          }
+        }
+      `
       return (
-        <div key={name} css={container}>
+        <div key={name} css={container}
+          onMouseEnter={() => {
+            const cur = {}
+            cur[name] = true
+            this.setState(cur)
+          }}
+          onMouseLeave={() => {
+            const cur = {}
+            cur[name] = false
+            this.setState(cur)
+          }}
+        >
           <div key={`${name}-info`} css={start}>
             <h2><Link to={`/projects/${repo.name}/`}>{name}</Link></h2>
             <Description text={repo.description} />
-            {license && license !== 'Other' ? <h4><Icon icon={faBalanceScale}/> {license}</h4> : null}
-            {stars ? <h4><Icon icon={faStar}/> {stars}</h4> : null }
             {url ? <ProjectButton href={url} type={type(repo)} /> : null}
             <Button inline={true} align="left" href={`/projects/${repo.name}/`}><Icon icon={faInfoCircle}/> More Info</Button>
           </div>
-          <div key={`${name}-image`} css={end}>
+          <div key={`${name}-image`} css={end} aria-hidden={!this.state[name]}>
             <Markdown alt={name} content={md} repo={repo} centerImages={false} filters={[removeBadges, firstImage]} />
           </div>
         </div>
