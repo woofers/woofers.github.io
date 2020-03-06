e11b7c4fcc555263cd2e6b6cda1066fdb6c57163
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-03-05 20:30:43 -0800

    Add Gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,10 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-transition-link`,
     `gatsby-plugin-emotion`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -28,8 +31,5 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
