38bd13a7c374293782bebb3b19abfe4951fb00dd
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-03-06 16:51:32 -0800

    Use list for projects

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -15,7 +15,7 @@ const style = css`
 const Avatar = p => {
   return (
     <div css={style}>
-      <img src="https://avatars3.githubusercontent.com/u/7284672?s=460&v=4" />
+      <img src="https://avatars3.githubusercontent.com/u/7284672?s=460&v=4" alt="Jaxson Van Doorn" />
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,24 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+const projects = [
+  'React Wavify',
+  'React PICO 8',
+  'React Dialog Polyfill',
+  'Ludum Dare Badges'
+]
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h4>Projects</h4>
-    <h1>React Wavify</h1>
-    <h1>React PICO 8</h1>
-    <h1>React Dialog Polyfill</h1>
-    <h1>Ludum Dare Badges</h1>
+    {
+      projects.map(project => (
+        <a key={project} href="#">
+          <h1>{project}</h1>
+        </a>
+      ))
+    }
   </Layout>
 )
 
