6c387e9a1489386d59a7b502e9a97b9e541ecdd3
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-03-06 21:45:41 -0800

    Change background color with page

--- src/components/page.js
+++ src/components/page.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import Layout from './layout'
 import Footer from './footer'
+import { css, Global } from '@emotion/core'
+
+const style = css`
+  body {
+    background: #fff;
+    color: #000;
+  }
+`
+
+const container = css`
+  margin-top: 50px;
+`
 
 const Page = ({ children }) => (
   <Layout outside={<Footer />}>
-    <main>{children}</main>
+    <Global styles={style} />
+    <main css={container}>{children}</main>
   </Layout>
 )
 
