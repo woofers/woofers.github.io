015c86def67e3615486ab0bc6375b1827023a5bc
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-03-08 16:57:51 -0700

    Add code highlighting to blog posts
    Also fixes footer placement

--- src/components/content.js
+++ src/components/content.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'prism-themes/themes/prism-duotone-space.css'
 
 const Content = p => {
   const { html, ...rest } = p
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,15 +1,86 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { graphql } from 'gatsby'
+import { css } from '@emotion/core'
+import Content from '../components/content'
+import Page from '../components/page'
+import SEO from '../components/seo'
 
-class PostTemplate extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    )
+const style = css`
+  a {
+    color: #f27052;
+    font-weight: 700;
+    &:hover {
+      color: rgba(242, 112, 82, 0.75);
+    }
   }
+`
+
+const PostTemplate = p => {
+  const { data } = p
+  const { orgContent, site } = data
+  const { html, metadata } = orgContent
+  const { title } = metadata
+  return (
+    <Page>
+      <SEO title={title} />
+      <div css={style}>
+        <Content html={html} />
+      </div>
+    </Page>
+  )
 }
 
 export default PostTemplate
 
-//export const pageQuery = graphql`
-//`
+export const pageQuery = graphql`
+  query($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        resume
+        social {
+          github {
+            name
+            link
+          }
+          stackoverflow {
+            name
+            link
+          }
+          twitter {
+            name
+            link
+            handle
+          }
+          linkedin {
+            name
+            link
+          }
+          email {
+            name
+            link
+          }
+        }
+      }
+    }
+    orgContent(fields: {slug: {eq: $slug}}) {
+      html
+      metadata {
+        title
+        date
+        icon
+        type
+        icon_mode
+        icon_type
+        ludum_dare
+        placeholder
+        game
+        landscape
+        lang
+      }
+      fields {
+        slug
+      }
+    }
+  }
+`
