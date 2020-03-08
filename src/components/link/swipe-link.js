62566e04ede99d59424a157ed6e639b1b6decf12
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-03-07 20:35:52 -0800

    Refactor links transitions

---
+++ src/components/link/swipe-link.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from './gen-link'
+
+const SwipeLink = p => (
+  <Link {...p} />
+)
+SwipeLink.defaultProps = {
+  duration: 0.8,
+  direction: 'up',
+  top: 'entry'
+}
+
+export default SwipeLink
