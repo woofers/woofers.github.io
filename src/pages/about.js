24c4736d392115ab88aaa3d8e4d043f84520cc40
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-03-07 21:31:46 -0800

    Add filler content to about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import Page from '../components/page'
 import SEO from '../components/seo'
+import { css } from '@emotion/core'
+
+const style = css`
+  a {
+    color: #f27052;
+    font-weight: 700;
+  }
+`
 
 const NotFoundPage = () => (
   <Page>
     <SEO title="About" />
     <h1>About</h1>
-    <p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fringilla orci eget diam imperdiet viverra in a tellus. Duis molestie vulputate lacus, vel efficitur quam hendrerit at. Maecenas molestie ullamcorper odio quis sagittis. Aliquam feugiat neque eget diam tristique laoreet. Sed sit amet sapien quis odio cursus convallis quis et est. Sed fermentum lacus convallis efficitur vehicula. Donec interdum mauris in risus lobortis vulputate. Pellentesque vitae felis et justo pulvinar convallis. Nullam ultrices congue magna eu cursus. Quisque laoreet turpis quis volutpat tincidunt. Curabitur eu dui ac ipsum feugiat fermentum. Fusce dui lacus, placerat quis dolor vel, hendrerit convallis ligula. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-
-Suspendisse egestas semper velit. Sed diam nisl, dapibus at gravida non, placerat sit amet velit. Curabitur mattis arcu ut metus eleifend egestas. Praesent feugiat nulla lectus, vel fringilla purus aliquam in. Ut sed finibus nunc. Maecenas consectetur, enim iaculis congue viverra, purus urna varius neque, vel ultricies tellus nunc id odio. Nulla auctor ligula id urna vehicula, in porta dui cursus. Aliquam tincidunt sed felis nec accumsan.
-
-Etiam a accumsan nisl, quis posuere metus. Fusce id fringilla purus, in egestas justo. Duis posuere nisl eget nisl consequat aliquam. Curabitur ut fringilla ante, vitae malesuada nunc. Cras maximus ornare erat. Vivamus dapibus sed justo a placerat. Nulla facilisi. Phasellus dapibus lacus ac dui dapibus, a porta urna pellentesque. Sed magna velit, scelerisque et lorem ac, ultricies consectetur orci. Donec feugiat magna sed pellentesque molestie. Cras aliquet enim nec rhoncus venenatis. Donec lobortis enim dapibus convallis tempus. Aliquam erat volutpat. Duis in est in nisi porttitor varius.
-    </p>
+    <div css={style}>
+      <p>My name is <strong>Jaxson Van Doorn</strong> &amp; I create software in <strong>Victoria, BC</strong>.</p><p>I enjoy doing both <i>frontend</i> &amp; <i>backend</i> web design work as-well as the occasional <i>game</i> for fun.</p><p>Most of my open-source projects these days are with <a href="https://reactjs.org/">React</a> + <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">JavaScript</a>, <a href="https://www.java.com">Java</a> or <a href="https://www.lua.org/">Lua</a>.</p><p>Currently studying my <strong>3rd</strong> year of <i>Computer Science</i> at <a href="https://uvic.ca">University of Victoria</a>.</p><p>You can find some of my <a href="/projects/">projects</a> on this site.</p><p>More projects can be found on my <a href="https://github.com/woofers">GitHub</a>, along with my <a href="https://jaxson.vandoorn.ca/resume/jaxsonvd-resume.pdf">resume</a>.</p><p>If you need to get in touch or have any question regarding the content on this site, I can be contacted through <a href="mailto:jaxson.vandoorn@gmail.com">jaxson.vandoorn@gmail.com</a>.</p>
+    </div>
   </Page>
 )
 
