f1ed26c862dcbb44e322ab32b6335bd0cd34a699
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-03-08 17:44:39 -0700

    Add projects from GitHub

--- src/templates/github.js
+++ src/templates/github.js
@@ -1,15 +1,86 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { graphql } from 'gatsby'
+import { css } from '@emotion/core'
+import { Markdown, removeBadges } from '../components/markdown'
+import Org from '../components/org'
+import Page from '../components/page'
+import SEO from '../components/seo'
+import dlv from 'dlv'
+import { mutateRepoNames } from '../utils/repo'
 
-class GitHubTemplate extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    )
+const style = css`
+  a {
+    color: #f27052;
+    font-weight: 700;
+    &:hover {
+      color: rgba(242, 112, 82, 0.75);
+    }
   }
+`
+
+const GitHubTemplate = p => {
+  const { data } = p
+  const { repositories, site } = data
+  const repo = repositories
+  const { siteMetadata } = site
+  const { exclude } = siteMetadata
+  const { fullName } = repo
+  mutateRepoNames([{ node: repo }], exclude)
+  const md = dlv(repo.readme, 'text')
+  const org = dlv(repo.readmeOrg, 'text')
+  return (
+    <Page>
+      <SEO title={fullName} />
+      <div css={style}>
+        { org && !md ?
+            <Org content={org} repo={repo} />
+          : <Markdown content={md} repo={repo} filters={[removeBadges]} />
+        }
+      </div>
+    </Page>
+  )
 }
 
 export default GitHubTemplate
 
-//export const pageQuery = graphql`
-//`
+export const pageQuery = graphql`
+  query($repo: String!) {
+    site {
+      siteMetadata {
+        title
+        resume
+        exclude
+        nav {
+          projects
+          blog
+          about
+        }
+      }
+    }
+    repositories(name: {eq: $repo}) {
+      name
+      description
+      url
+      homepage
+      stars {
+        totalCount
+      }
+      license {
+        name
+      }
+      topics {
+        nodes {
+          topic {
+            name
+          }
+        }
+      }
+      readme {
+        text
+      }
+      readmeOrg {
+        text
+      }
+    }
+  }
+`
