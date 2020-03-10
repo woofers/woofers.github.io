81c1e7915331f96fa7635a38269f1ba40fff43ee
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-03-09 22:19:10 -0700

    Add template pages

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Page from '../components/page'
+import SEO from '../components/seo'
+
+const Blog = p => {
+  return (
+    <Page>
+      <SEO title="Blog" />
+    </Page>
+  )
+}
+
+export default Blog
---
+++ src/pages/projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Page from '../components/page'
+import SEO from '../components/seo'
+
+const Projects = p => {
+  return (
+    <Page>
+      <SEO title="Projects" />
+    </Page>
+  )
+}
+
+export default Projects
