e3f5c80f293e158241cd76083822174df2d616b5
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-04-29 14:51:37 -0700

    Re-direct 404 page to splash page
    Also removes the history entry of the invalid page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,9 @@
-import React from 'react'
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import { useEffect } from 'react'
+import { navigate } from 'gatsby'
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
-
-export default NotFoundPage
+export default () => {
+  useEffect(() => {
+    navigate('/', { replace: true })
+  }, [])
+  return null
+}
