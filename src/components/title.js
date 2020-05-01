f663ea381b2fec22dfd559c0aed00acf20c1b6d7
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-05-01 09:51:22 -0700

    Left justify title on projects and blog posts

--- src/components/title.js
+++ src/components/title.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import { css } from '@emotion/core'
-
-const align = css`
-  text-align: right;
-`
 
 const Title = p => {
   const { children, ...rest } = p
   return (
-    <h1 css={align} {...rest}>
+    <h1 {...rest}>
       {children}
     </h1>
   )
