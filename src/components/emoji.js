153ca3fa51fa044a3b09e0445da6502937302f1c
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-05-05 13:35:54 -0700

    Use SVG emojis

--- src/components/emoji.js
+++ src/components/emoji.js
@@ -1,5 +1,52 @@
 import React from 'react'
 import { css } from '@emotion/core'
+import {
+  Fox,
+  Whale,
+  DogFace,
+  Snake,
+  WaterWave,
+  DesertIsland,
+  GameDie,
+  Joystick,
+  AlienMonster,
+  Trophy,
+  ManAstronaut,
+  Telescope,
+  MilkyWay,
+  Cyclone,
+  InputLatinUppercase,
+  Scroll,
+  ComputerMouse,
+  SpeechBalloon,
+  VideoGame,
+  Paintbrush,
+  Wrench
+} from 'noto-emoji-react'
+
+const table = {
+  '🦊': Fox,
+  '🐋': Whale,
+  '🐶': DogFace,
+  '🐍': Snake,
+  '🌊': WaterWave,
+  '🏝': DesertIsland,
+  '🎲': GameDie,
+  '🕹': Joystick,
+  '👾': AlienMonster,
+  '🏆': Trophy,
+  '👨‍🚀': ManAstronaut,
+  '🔭': Telescope,
+  '🌌': MilkyWay,
+  '🌀': Cyclone,
+  '🔠': InputLatinUppercase,
+  '📜': Scroll,
+  '🖱': ComputerMouse,
+  '💬': SpeechBalloon,
+  '🎮': VideoGame,
+  '🖌': Paintbrush,
+  '🔧': Wrench
+}
 
 const img = code => `//github.githubassets.com/images/icons/emoji/unicode/${code}.png`
 const border = css`
@@ -7,8 +54,13 @@ const border = css`
   margin-bottom: 0;
 `
 
-const Emoji = ({ emoji, size }) =>
-  (<img alt="" css={border} width={size} height={size} src={img(emoji.codePointAt(0).toString(16).toLowerCase())} />)
+const Emoji = p => {
+  const { emoji, size, children, ...rest } = p
+  const noVs = unescape(escape(emoji).replace('%uFE0F', ''))
+  const Icon = table[noVs]
+  if (Icon) return <Icon {...rest} width={size} height={size} />
+  return <img alt="" css={border} width={size} height={size} src={img(noVs.codePointAt(0).toString(16).toLowerCase())} />
+}
 
 Emoji.defaultProps = {
   emoji: '👾',
