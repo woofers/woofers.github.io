f2d0898ae6c75e3220eb3ed440909f3e4aebfb65
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-09-11 16:53:32 -0700

    Use div for avatar image

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -4,22 +4,19 @@ import { css } from '@emotion/core'
 const Avatar = p => {
   const { size, ...rest } = p
   const style = theme => css`
-    > img {
-      border-radius: 50%;
-      border: 1.5px solid ${theme.colors.text.light};
-    }
+    border-radius: 50%;
+    background: #f58b71;
+    background-image: url(https://avatars3.githubusercontent.com/u/7284672?s=260&v=4);
     width: ${size};
     height: ${size};
+    background-size: 100%;
+    border-radius: 50%;
+    box-shadow: 0.3px -0.4px 0px 2px ${theme.colors.text.light};
     min-width: ${size};
     min-height: ${size};
-    user-select: none;
   `
   return (
-    <div css={style} {...rest}>
-      <img src="https://avatars3.githubusercontent.com/u/7284672?s=260&v=4"
-        alt="Jaxson Van Doorn"
-        draggable="false"
-      />
+    <div css={style} role="img" {...rest} aria-label="Jaxson Van Doorn">
     </div>
   )
 }
