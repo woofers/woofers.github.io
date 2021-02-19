9d00090d7bf6697f11d1f2eba07df1aa3667f3c0
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-02-19 15:11:01 -0800

    Clean index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import Splash from '../components/splash'
-import { FadeLink as Link } from '../components/link'
 import SEO from '../components/seo'
 import { css } from '@emotion/react'
 import { graphql, useStaticQuery } from 'gatsby'
 import { mutateRepoNames } from '../utils/repo'
-import Description from '../components/description'
-import { small, large } from '../styles/text'
 
 const side = theme => css`
   justify-content: space-between;
@@ -47,66 +44,6 @@ const side = theme => css`
   }
 `
 
-const desc = css`
-  > a {
-    > div {
-      > h3 {
-        margin-bottom: 5px;
-      }
-      & > div {
-        height: 0;
-        opacity: 0;
-        overflow: hidden;
-        margin-bottom: 15px;
-      }
-    }
-  }
-  > a:hover, > a:focus {
-    > div {
-      & > div {
-        opacity: 1;
-        height: 25px;
-        overflow: hidden;
-      }
-    }
-  }
-  transition: height 0.3s cubic-bezier(0.165, 0.84, 0.44, 1) 0s, opacity 0.3s;
-`
-
-
-const Projects = p => {
-  const { projects, children, ...rest } = p
-  const noChild = !React.Children.count(children)
-  const height = theme => css`
-    @media (min-width: ${theme.breakpoints.normal.breakpoint}) {
-      margin-top: ${noChild ? '50px' : '0'};
-      height: ${noChild ? '390px' : '440px'};
-    }
-  `
-  return (
-    <div {...rest} css={height}>
-      {children}
-      {
-        projects.map(project => {
-          const description = `${project.name}-description`
-          return (
-            <div key={project.name} css={desc}>
-              <Link to={`/projects/${project.name}/`} aria-describedby={description} aria-label={project.fullName}>
-                <div>
-                  <h3 css={large}>{project.fullName}</h3>
-                  <div css={desc}>
-                    <Description id={description} text={project.description} />
-                  </div>
-                </div>
-              </Link>
-            </div>
-          )
-        })
-      }
-    </div>
-  )
-}
-
 const IndexPage = () => {
   const data = useStaticQuery(graphql`{ ...GitHubProjects }`)
   const { site } = data
@@ -115,16 +52,11 @@ const IndexPage = () => {
   let repos = data.allRepositories.edges
   mutateRepoNames(repos, exclude)
   repos = repos.map(({ node }) => node)
-  const right = repos.filter(repo => !!repo.fullName)
-  const left = right.splice(0, Math.ceil(right.length / 2))
   return (
     <Splash>
       <SEO />
       <div css={side}>
-        <Projects projects={left}>
-          <h2 css={small}>Projects</h2>
-        </Projects>
-        <Projects projects={right} />
+
       </div>
     </Splash>
   )
