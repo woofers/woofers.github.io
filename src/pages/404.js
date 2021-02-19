22ced0df5487468424f82388b49e7d5b65068fd3
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-02-19 15:03:36 -0800

    Use named function for redirect

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,11 @@
 import { useEffect } from 'react'
 import { navigate } from 'gatsby'
 
-export default () => {
+const NotFound = () => {
   useEffect(() => {
     navigate('/', { replace: true })
   }, [])
   return null
 }
+
+export default NotFound
