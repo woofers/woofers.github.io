4c6f1d1c5044dd77990455c39de244684d4318a6
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-03-05 17:51:32 -0800

    More input box

--- src/components/input.js
+++ src/components/input.js
@@ -1,20 +1,41 @@
 import React from 'react'
 import { css } from '@emotion/react'
 
+const placeholder = css`
+  color: #c8ccd0;
+  opacity: 1;
+`
+
 const style = css`
+  width: 100%;
   font-size: 30px;
   border: none;
-  padding: 20px;
-  background: #fcf3ec;
-  color: #777f9e;
+  padding: 15px 20px;
+  background: #fbf7f3;
+  color: #2b3044;
   font-weight: 700;
   border-radius: 5px;
   transition: box-shadow 0.08s ease-in-out;
-  box-shadow: 0 0 #ff7170;
+  box-shadow: 0 4px #c8ccd0;
   &:focus {
     outline: none;
-    box-shadow: 5px 10px #ff7170;
+    box-shadow: 0 4px #ff9b74;
   }
+
+  ::-webkit-input-placeholder {
+    ${placeholder}
+  }
+  ::-moz-placeholder {
+    ${placeholder}
+  }
+  ::-ms-placeholder {
+    ${placeholder}
+  }
+  ::placeholder {
+    ${placeholder}
+  }
+
+  font-family: 'Hammersmith One',sans-serif;
 `
 
 const Input = p => {
