de4b900d62483355629159b1d01ab239b5f2ae4b
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-03-19 21:51:50 -0700

    Blink

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { keyframes, css } from '@emotion/react'
 
-const blink = keyframes`
+const blinkAnimation = keyframes`
   0% {
     opacity: 0;
   }
@@ -18,27 +18,64 @@ const hello = css`
   width: 100%;
 `
 
-const highlight = css`
+const clip = css`
   -webkit-text-fill-color: transparent;
   -webkit-box-decoration-break: clone;
-  background: -webkit-linear-gradient(-90deg,#ff7170,#ffd57f);
   -webkit-background-clip: text;
   background-clip: text;
 `
 
+const highlight = css`
+  background: -webkit-linear-gradient(-90deg,#ff7170,#ffd57f);
+  ${clip}
+`
+
+const blink = css`
+  animation: ${blinkAnimation} 2s infinite;
+`
+
+const name = css`
+  ${highlight}
+  filter: brightness(0);
+  transition: filter 0.2s cubic-bezier(0, 0, 0.1, 1) 0ms;
+`
+
+const bright = css`
+  filter: brightness(1);
+`
+
 const cursor = css`
   ${highlight}
-  animation: ${blink} 2s infinite;
   display: inline-block;
+  width: 30px;
+  transition: transform 0.2s cubic-bezier(0, 0, 0.1, 1) 0ms;
+`
+
+const italic = css`
   transform: scale(1, 1.25) rotate(8deg) translate(5px, 8px);
   font-style: italic;
-  width: 30px;
 `
+
+const content = 'jaxs.on'
+
+const fadeTicks = 8
+const blinkTicks = fadeTicks + 2
 
 const Logo = p => {
   const { children, ...rest } = p
+  const [index, setIndex] = useState(0)
+  const isTyping = index < content.length
+  const isBlink = index < content.length + blinkTicks
+  const isFade = index > content.length + fadeTicks
+  const ifTyping = style => isTyping ? style : null
+  const ifBlink = style => isBlink ? style : null
+  const ifFade = style => isFade ? style : null
+  setTimeout(() => setIndex(index + 1), 150)
   return (
-    <h1 css={hello}>jaxs.on<span css={cursor}>l</span></h1>
+    <h1 css={hello}>
+      <span css={[name, ifFade(bright)]}>{content.substring(0, index)}</span>
+      <span css={[cursor, ifTyping(italic), ifBlink(blink)]}>l</span>
+    </h1>
   )
 }
 
