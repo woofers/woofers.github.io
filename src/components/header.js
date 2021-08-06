13e6ab2ac8985be24a4eb658d61e579e150d3259
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-08-05 21:35:57 -0700

    Add menu transition for overlay gradient

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,26 @@ import withLocation from './with-location'
 import Title from './title'
 import { Link } from 'gatsby'
 
+const head = arr => arr[0]
+const last = arr => arr[arr.length - 1]
+
 const Wrapper = styled.header``
 
-const alpha = 0.36
+const color = (alpha = 1) => `rgba(255, 255, 255, ${alpha})`
+const edge = color(1)
+const fade = color(0.36)
+const none = 'rgba(0, 0, 0, 0)'
+
+const makeGradient = (left, right) => `
+  linear-gradient(
+    90deg,
+    ${!left ? edge : none} 0%,
+    ${!left ? fade : none} 5%,
+    ${none} 50%,
+    ${!right ? fade : none} 95%,
+    ${!right ? edge : none} 100%
+  );
+`
 
 const Menu = styled.div`
   display: flex;
@@ -16,50 +33,46 @@ const Menu = styled.div`
   &:after {
     content: '';
     pointer-events: none;
-    background-image: linear-gradient(
-      90deg,
-      #fff 0%,
-      rgba(255, 255, 255, ${alpha}) 5%,
-      #ff00 50%,
-      rgba(255, 255, 255, ${alpha}) 95%,
-      #fff 100%
-    );
+    background-image: ${props => makeGradient(props.left, props.right)};
     position: absolute;
     height: 100%;
     width: 100%;
   }
 `
 
+const items = [
+  {
+    to: '/about/',
+    children: 'Me'
+  },
+  {
+    to: '/',
+    children: 'Work'
+  },
+  {
+    to: '/me/',
+    children: 'Contact'
+  }
+]
+
 const Header = props => {
   const { location } = props
   const path = location?.pathname
   return (
     <Wrapper>
-      <Menu>
-        <Title
-          to="/about/"
-          as={Link}
-          aria-current={path === '/about/' ? 'page' : undefined}
-          color="#c9c9c9"
-        >
-          Me
-        </Title>
-        <Title
-          to="/"
-          as={Link}
-          aria-current={path === '/' ? 'page' : undefined}
-          color="#c9c9c9"
-        >
-          Work
-        </Title>
-        <Title
-          to="/me/"
-          as={Link}
-          aria-current={path === '/me/' ? 'page' : undefined}
-          color="#c9c9c9"
-        >
-          Contact
-        </Title>
+      <Menu left={head(items).to === path} right={last(items).to === path}>
+        {items.map(({ children, to, ...rest }) => (
+          <Title
+            {...rest}
+            key={`menu-link-${to}`}
+            to={to}
+            as={Link}
+            aria-current={path === to ? 'page' : undefined}
+            color="#c9c9c9"
+          >
+            {children}
+          </Title>
+          ))}
       </Menu>
     </Wrapper>
   )
