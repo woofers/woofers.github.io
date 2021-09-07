d1759408357612e8db32fecd09a9d67a4ca0eebc
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-09-06 17:30:49 -0700

    Add input style

--- src/components/input.js
+++ src/components/input.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import { styled } from 'emotion'
+
+const Field = styled.input`
+  font-size: 16px;
+  font-weight: 700;
+  font-family: 'Cabin', sans-serif;
+  padding: 12px;
+  margin: 12px;
+  border: none;
+  outline: none;
+  width: 100%;
+  max-width: 240px;
+  border-radius: 8px;
+  background: #fff;
+  color: #646b8c;
+  box-shadow: inset 0 0 0 var(--border-width, 1px) var(--border, #e1e6f9);
+  transition: box-shadow 0.2s;
+  &::placeholder {
+    color: #bbc1e1;
+    opacity: 1;
+  }
+  &:focus {
+    --border-width: 1.5px;
+    --border: #275EFE;
+  }
+`
 
 const Input = ({ children, ...rest }) => {
-  return <input type="text" {...rest} />
+  return <Field type="text" {...rest} />
 }
 
 export default Input
