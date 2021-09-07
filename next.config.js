d28fd877f427b5f36f179bbc2b9d2f908cfa812c
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-09-07 16:48:46 -0700

    Enable ESLint

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const ESLintPlugin = require('eslint-webpack-plugin')
+
 module.exports = {
   distDir: 'build',
   webpack5: true,
   reactStrictMode: true,
   webpack(config) {
+    config.plugins.push(new ESLintPlugin())
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack', 'file-loader'],
