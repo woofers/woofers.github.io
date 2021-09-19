b32ba9225950f2254a24a11935b8b7fffd99a333
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-09-19 12:42:07 -0700

    Add org parse

--- src/components/org.js
+++ src/components/org.js
@@ -1,26 +1,23 @@
 import toHast from 'oast-to-hast'
-import hastToHtml from 'hast-util-to-html'
+import {toHtml} from 'hast-util-to-html'
 import { parse as orga } from 'orga'
 import { toGitHubLink } from 'utils/link'
 import Content from './content'
+//import reorg from '@orgajs/reorg'
+//import stream from 'unified-stream'
+//import mutate from '@orgajs/reorg-rehype'
+//import html from 'rehype-stringify'
+
 
 const Org = p => {
+  //const processor = reorg().use(mutate).use(html)
   const parse = content => {
-    const link =
-      ({ h, u }) =>
-      node => {
-        const props = { href: node.value }
-        if (node.protocol === 'file') {
-          node.value = toGitHubLink(node.value, p.repo.name)
-          return h('img', { src: node.value, alt: node.description })()
-        }
-        return h(`a`, props)(u(`text`, node.description))
-      }
     const ast = orga(content)
-    const hast = toHast(ast, { highlight: true, handlers: { link } })
-    return hastToHtml(hast, { allowDangerousHtml: true })
+    const hast = toHast(ast, { highlight: true })
+    return toHtml(hast, { allowDangerousHtml: true })
   }
 
+    //console.log(processor.processSync(p.content).result)
   return (
     <div>
       <Content html={parse(p.content)} />
