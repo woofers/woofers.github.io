e5440dc8cdfbb44d071f18f2923a5b0a779389e5
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-09-19 12:49:00 -0700

    Use rehype

--- src/components/org.js
+++ src/components/org.js
@@ -3,24 +3,17 @@ import {toHtml} from 'hast-util-to-html'
 import { parse as orga } from 'orga'
 import { toGitHubLink } from 'utils/link'
 import Content from './content'
-//import reorg from '@orgajs/reorg'
-//import stream from 'unified-stream'
-//import mutate from '@orgajs/reorg-rehype'
-//import html from 'rehype-stringify'
+import reorg from '@orgajs/reorg'
+import stream from 'unified-stream'
+import mutate from '@orgajs/reorg-rehype'
+import html from 'rehype-stringify'
 
 
 const Org = p => {
-  //const processor = reorg().use(mutate).use(html)
-  const parse = content => {
-    const ast = orga(content)
-    const hast = toHast(ast, { highlight: true })
-    return toHtml(hast, { allowDangerousHtml: true })
-  }
-
-    //console.log(processor.processSync(p.content).result)
+  const processor = reorg().use(mutate).use(html)
   return (
     <div>
-      <Content html={parse(p.content)} />
+      <Content html={processor.processSync(p.content).contents} />
     </div>
   )
 }
