889a04713aa3e8a5b99e4149810fb8f3e0290435
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-09-19 11:30:43 -0700

    Use new markdown plugin

--- src/components/markdown.js
+++ src/components/markdown.js
@@ -1,67 +1,59 @@
-import remark from 'remark'
-import remark2react from 'remark-react'
-import unist from 'unist-builder'
-import visit from 'unist-util-visit'
-import filter from 'unist-util-filter'
-import { selectAll, select } from 'unist-util-select'
+import React, { useEffect, useState } from 'react'
+//import unist from 'unist-builder'
+import { visit } from 'unist-util-visit'
+import { filter } from 'unist-util-filter'
+//import { selectAll, select } from 'unist-util-select'
 import { toGitHubLink } from 'utils/link'
-import github from 'hast-util-sanitize/lib/github'
-import merge from 'lodash.merge'
+//import github from 'hast-util-sanitize/lib/github'
+//import merge from 'lodash.merge'
 import Link from 'link'
 import { styled } from 'emotion'
 import { css } from '@emotion/react'
-import { lowlight as low } from 'lowlight'
-import 'lowlight/lib/core.js'
-import 'lowlight/lib/all.js'
 
-var m = function n(o) {
-    return Object.keys(o).reduce(function(newObj, key) {
-        var value = o[key];
-        const newKey = key === 'properties' ? 'hProperties' : key
-        const againKey = key === 'children' ? 'hChildren' : key
-        if (value !== null && typeof value === 'object') {
-          newObj[againKey] = n(value);
-        } else {
-          newObj[againKey] = value;
-        }
-        return newObj;
-    }, {});
-};
+import {unified} from 'unified'
+import remarkParse from 'remark-parse'
+import remarkSlug from 'remark-slug'
+import remarkToc from 'remark-toc'
+import remarkGithub from 'remark-github'
+import remarkRehype from 'remark-rehype'
+import rehypeHighlight from 'rehype-highlight'
+import rehypeReact from 'rehype-react'
+import javascript from 'highlight.js/lib/languages/javascript'
 
 function attacher({include, exclude, prefix} = {}) {
-  return (ast) => visit(ast, 'code', visitor)
+  //return (ast) => visit(ast, 'code', visitor)
 
-  function visitor(node) {
-    let {lang, data} = node
+  //function visitor(node) {
+  //  let {lang, data} = node
 
-    if (
-      !lang ||
-      (include && !include.includes(lang)) ||
-      (exclude && exclude.includes(lang))
-    ) {
-      return
-    }
+  //  if (
+  //    !lang ||
+  //    (include && !include.includes(lang)) ||
+  //    (exclude && exclude.includes(lang))
+  //  ) {
+  //    return
+  //  }
 
-    if (!data) {
-      data = {}
-      node.data = data
-    }
+  //  if (!data) {
+  //    data = {}
+  //    node.data = data
+  //  }
 
-    if (!data.hProperties) {
-      data.hProperties = {}
-    }
+  //  if (!data.hProperties) {
+  //    data.hProperties = {}
+  //  }
 
-    data.hChildren = low.highlight(lang, node.value, {prefix: 'hljs-'}).children
-    data.hProperties.className = [
-      'hljs',
-      ...(data.hProperties.className || []),
-      'language-' + lang
-    ]
-    console.log(node)
-  }
+  //  data.hChildren = low.highlight(lang, node.value, {prefix: 'hljs-'}).children
+  //  data.hProperties.className = [
+  //    'hljs',
+  //    ...(data.hProperties.className || []),
+  //    'language-' + lang
+  //  ]
+  //  console.log(node)
+  //}
 }
 
-const schema = merge(github, { attributes: { '*': ['className', 'type'] } })
+//const schema = merge(github, { attributes: { '*': ['className', 'type'] } })
 
 const Wrapper = styled.div`
   h1:first-of-type {
@@ -69,22 +61,22 @@ const Wrapper = styled.div`
   }
 `
 
-export const onlyImages = () => {
-  return (tree) => unist('paragraph', selectAll('image', tree))
-}
-
-export const firstImage = () => {
-  return (tree) => {
-    const image = select('image', tree)
-    return image ? image : unist('paragraph', '')
-  }
-}
+//export const onlyImages = () => {
+//  return (tree) => unist('paragraph', selectAll('image', tree))
+//}
+//
+//export const firstImage = () => {
+//  return (tree) => {
+//    const image = select('image', tree)
+//    return image ? image : unist('paragraph', '')
+//  }
+//}
 
 export const removeBadges = () => {
   return (tree) => {
     return filter(tree, node => {
       const child = node.children ? node.children[0] : ''
-      return !(node.type === 'link' && child && child.type === 'image')
+      return !(node.tagName === 'a' && child && child.tagName === 'img')
     })
   }
 }
@@ -95,11 +87,11 @@ export const Markdown = p => {
     const visitor = node => {
       const githubLink = /^\.\//g
       const setAlt = alt => {
-        node.title = alt
-        node.alt = alt
+        node.properties.title = alt
+        node.properties.alt = alt
       }
-      if (githubLink.test(node.url)) {
-        node.url = toGitHubLink(node.url, p.repo.name)
+      if (githubLink.test(node.properties.src)) {
+        node.properties.src = toGitHubLink(node.properties.src, p.repo.name)
       }
       if (options.alt) {
         setAlt(options.alt)
@@ -108,20 +100,22 @@ export const Markdown = p => {
         setAlt(`${p.repo.fullName} ${node.title}`)
       }
     }
-    return (tree) => visit(tree, ['image', 'link', 'linkReference'], visitor)
+    return (tree) => visit(tree, node => node.tagName === 'img', visitor)
   }
   const content = () => {
-    let md = remark().use(remark2react, {
-      remarkReactComponents: {
-        a: p => <Link href={p.href} underline>{p.children}</Link>
-      },
-      sanitize: schema
-    }).use(attacher)
-    md = md.use(() => links({ alt: p.alt }))
+    let processor = unified()
+      .use(remarkParse)
+      .use(remarkSlug)
+      .use(remarkToc)
+      .use(remarkGithub, {repository: `woofers/${p.repo.name}`})
+      .use(remarkRehype)
+      .use(rehypeHighlight, { languages: { javascript } })
+      .use(rehypeReact, {createElement: React.createElement})
+    processor = processor.use(() => links({ alt: p.alt }))
     for (const filter of p.filters) {
-      md = md.use(filter)
+      processor = processor.use(filter)
     }
-    return md.processSync(p.content).result
+    return processor.processSync(p.content).result
   }
   return (
     <Wrapper>
