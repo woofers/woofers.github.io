f266823d10206e3930536baa7ff07799aef6c1e5
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-09-23 22:11:11 -0700

    Import content from blog files

---
+++ src/pages/blog/[post].js
@@ -0,0 +1,48 @@
+import React from 'react'
+import path from 'path'
+import fs from 'fs-extra'
+
+const Post = ({ post }) => {
+  console.log(post)
+  return (
+    <>
+      hello
+    </>
+  )
+}
+
+export const getStaticProps = async ({ params, ...rest }) => {
+  const { post } = params
+  const getPost = () => {
+    const filePath = path.join('./content/blog', `${post}.org`)
+    const { ext, name } = path.parse(filePath)
+    if (ext.startsWith('.org')) {
+      try {
+        const content = fs.readFileSync(filePath, 'utf8')
+        const post = {
+          content,
+          date: '',
+          post: filePath
+            .replace(/^content\/blog\//, '')
+            .replace(/.org?$/, '')
+        }
+        return post
+      }
+      catch (e) {
+        console.log(e)
+        return {}
+      }
+    }
+  }
+  return { props: { post: getPost()  } }
+}
+
+export const getStaticPaths = () => {
+  const files = fs.readdirSync('./content/blog').map(name => name.replace(/.org?$/, ''))
+  return {
+    paths: files.map(post => ({ params: { post } })),
+    fallback: false
+  }
+}
+
+export default Post
