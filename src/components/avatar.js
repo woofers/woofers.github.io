08d91068373ac3398a777fcc4d117fd33f2c7a00
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-10-10 13:50:13 -0700

    Use next image

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { styled, useTheme, useMediaQuery } from 'emotion'
 import { motion, AnimatePresence } from 'framer-motion'
 import Image from 'next/image'
-import jaxson from 'images/IMG_0252.jpg'
 
 const Wrapper = styled(motion.div)`
   display: block;
@@ -21,6 +20,13 @@ const Wrapper = styled(motion.div)`
   }
 `
 
+const isDev = process.env.NODE_ENV !== 'production'
+const url = !isDev ? 'https://jaxs.onl' : `http://localhost:3000`
+const loader = ({ src, width, quality }) => {
+  return `${url}/me/${src}_${width}_${quality}.webp`
+}
+
+
 const Avatar = ({ intro = true, show = true, ...rest }) => {
   const theme = useTheme()
   const mobile = useMediaQuery(`@media only screen and (max-width: ${theme.breakpoints.large.breakpoint})`)
@@ -34,7 +40,7 @@ const Avatar = ({ intro = true, show = true, ...rest }) => {
           transition={{ duration: intro ? 0.5 : 0.15, delay: intro ? 1.3 : 0 }}
           {...rest}
         >
-          <Image src={jaxson} quality={80} sizes={!mobile ? '1200px' : '180px'}layout="responsive" placeholder="blur" alt="Photo of Jaxson in a grey hoodie and red plaid shirt" />
+          <Image src="me" width="3024px" height="4032px" blurDataURL={loader('me', 8, 80)} loader={loader} quality={80} sizes={!mobile ? '1200px' : '180px'} layout="responsive" placeholder="blur" alt="Photo of Jaxson in a grey hoodie and red plaid shirt" />
         </Wrapper>
       )}
     </AnimatePresence>
