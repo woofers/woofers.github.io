f8b5e37cec56eb049727692ee2ddcb9c5dee994e
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-12-04 15:20:41 -0800

    Bump to v5.8.0
    New Features:
    
    - Use transparent avatar image

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { css } from '@emotion/react'
+import image from '../images/jaxson.webp'
 
 const Avatar = p => {
   const { size, fallback, ...rest } = p
   const style = theme => css`
     background: ${fallback};
-    background-image: url(https://avatars3.githubusercontent.com/u/7284672?s=260&v=4);
+    background-image: url(${image});
+    background-position: 100% 50%;
     transition: background-image 0.25s ease-in-out;
     width: ${size};
     height: ${size};
