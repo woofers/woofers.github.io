c782eeca5c89e7b8c73dd593a183db28c8346b29
Jaxson Van Doorn <woofers1tt@gmail.com>
2024-06-20 04:04:05 +0000

    Setup breakpoints

--- src/components/styled/container.tsx
+++ src/components/styled/container.tsx
@@ -13,7 +13,7 @@ const Container: React.FC<{
     flexGrow={flexGrow}
     className={clsx(
       'flex flex-col mx-auto w-full max-w-container',
-      container === 'page' ? '[max-width:960px]' : '[max-width:1400px]',
+      container === 'page' ? '[max-width:960px]' : '[max-width:1120px] xl:[max-width:1280px] 2xl:[max-width:1400px] 3xl:[max-width:1600px]',
       (padding === 'xy' || padding === 'x') && 'px-4 sm:px-6 lg:px-8',
       (padding === 'xy' || padding === 'y') && 'py-2 sm:py-3 lg:py-4',
       className
