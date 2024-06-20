d77913d278e6a44e48779d1b890b814a0ecc3574
woofers <woofers1tt@gmail.com>
2024-06-20 04:28:18 +0000

    v8.0.0

---
+++ _next/static/YI2or9E7Af9PSgca9Ibh7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-5a2270329d10c814.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
